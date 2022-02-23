694b529a14c49e2cb9d5cf2e51d5ef13988d287e
Crisdj13 <99026164+crisdj13@users.noreply.github.com>
2022-02-23 20:38:23 +0000

    complete

--- bus.py
+++ bus.py
@@ -4,4 +4,56 @@
 #
 #######################################################
 
+'''
+depature = input("When are you departing (HH:MM) ")
+aHour=int(depature[0:2])
+aMin=int(depature[3:])
+aSec=0
+distance= int(input("How far are you traveling (km) "))
+stops= int(input("How many stops will the bus do "))
 
+
+#Compute the arrival time assuming the bus travels, on average, at 40 km/h, and that each stop takes 30 seconds.  Your input departure time can just be in hours/minutes, but your output should include hours, minutes, and seconds.
+
+
+#Find total time of travel
+#calculating whether to add stops time in mins or seconds
+if stops%2==0:
+  stopTime= (30* stops)/60
+  aMin+=stopTime
+else:
+  stopTime= (30* stops)//60
+  stopTimeSeconds= (30* stops)%60
+  aMin+=stopTime
+  aSec+=stopTimeSeconds
+# calculating if whether to add distance in hours or 
+#in mins 
+
+dMins= ((distance/40)*60) 
+
+if (dMins%1) != 0 :    # TAKE CARE OF SECS FROM KM TRAVEL
+  aSec += (dMins%1)*60
+
+
+if dMins<60:
+  aMin+=dMins
+else:
+  aHour+= dMins//60
+  aMin+= dMins%60
+# handle kilometers 
+
+
+
+#Handle outside condtions 
+if aSec>59:
+  aMin+=1
+  aSec= aSec%60
+if aMin>59:
+  aHour+=1
+  aMin= aMin%60
+if aHour>=24:
+  aHour= 0 + aHour%24
+
+print(f"You will arrive at {int(aHour):02d}:{int(aMin):02d}:{int(aSec):02d} GMT ")
+
+'''
--- face.py
+++ face.py
@@ -5,4 +5,71 @@
 #######################################################
 
 import turtle
+import random 
+
+c = turtle.Turtle()
+# I am not an artist 
+#SO I WILL BE DOING ABSTRACT ART
+def drawS():
+  for i in range(4):
+    c.forward(150)
+    c.left(90)
+  c.forward(150)
+
+sc = turtle.Screen()
+sc.mode('world')
+c.getscreen()
+sc.title("FINISH LINE OR ABSTRACT ????")
+sc.setworldcoordinates(0, 0, 1100, 1100)
+
+c.speed('fastest')
+
+for i in range(8):
+  c.up()
+  c.setpos(-100, 150 * i)
+
+  c.down()
+  for j in range(8):
+    if (i +j)%2==0:
+      col= 'black'
+    else:
+      col='white'
+    c.fillcolor(col)
+    c.begin_fill()
+    drawS()
+    c.end_fill()
+#c.hideturtle()
+#turtle.done()
+
+
+
+def doAnimation(radius,num,x,y):
+  c.penup()
+  c.setpos(x,y)
+  c.pendown()  
+  r= radius
+  n=num
+  
+  for i in range(1, n):
+    x,y,z= random.random(),random.random(), random.random()
+    c.fillcolor(x,y,z)
+    c.begin_fill()
+    c.circle(r * i)
+    c.end_fill()
+doAnimation(20,10,500,500)
+doAnimation(15,10,200,200)
+doAnimation(10,10,800,800)
+doAnimation(20,10,100,100)
+doAnimation(20,10,100,200)
+doAnimation(20,20,50,800)
+#doAnimation(20,50,900,10)
+#doAnimation(20,400,500,10)
+#party time 
+for i in range(1000):
+  x= random.randrange(1,1000)
+  y=random.randrange(1,1000)
+  doAnimation(5,10,x,y)
+ 
+turtle.Screen().exitonclick()
+
 
